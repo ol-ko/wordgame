80184570f26d2d941eb2f984574b293726444e55
Olga Skurativska <olga.skurativska@unic.com>
2016-02-05 13:20:20 +0100

    Added score calculation logic to a game module

--- app/game/controllers/game.controller.js
+++ app/game/controllers/game.controller.js
@@ -2,7 +2,13 @@ angular.module("game").controller('GameController', ['$scope', 'Words',
 	function ($scope, Words) {
 
 		var wordsPull = [],
-			activeWord;
+			activeWord,
+			wordMaxScore,
+			removedCharsCounter,
+			previousAttemptWordLength;
+
+		$scope.scoreHistory = [];
+		$scope.totalScore = 0;
 
 		// Fisher-Yates array shuffle
 		var shuffleArray = function (array) {
@@ -36,13 +42,42 @@ angular.module("game").controller('GameController', ['$scope', 'Words',
 			// obfuscate word
 			$scope.obfuscatedWord = shuffleString(activeWord);
 
+			// init current puzzle
 			$scope.attempt = '';
+			wordMaxScore = Math.floor(Math.pow(1.95, activeWord.length / 3));
+			removedCharsCounter = 0;
+			previousAttemptWordLength = 0;
+		};
+
+		// Calculates and records word score
+		var recordWordScore = function () {
+			var wordScore = wordMaxScore - removedCharsCounter;
+
+			wordScore = wordScore < 0 ? 0 : wordScore;
+
+			$scope.totalScore += wordScore;
+			$scope.scoreHistory.push({
+				word: activeWord,
+				score: wordScore,
+				max: wordMaxScore,
+				corrections: removedCharsCounter
+			});
 		};
 
 		$scope.handleUserAttempt = function () {
+			var attemptLength = $scope.attempt.length;
+
+			// if previous attempt is shorter than current - smth has been deleted
+			if (previousAttemptWordLength > attemptLength) {
+				removedCharsCounter += 1;
+				$scope.removedCharsCounter = removedCharsCounter;
+			}
+
+			previousAttemptWordLength = attemptLength;
+
 			// user has guessed
 			if ($scope.attempt === activeWord) {
-				// TODO: record score
+				recordWordScore();
 				initNewPuzzle();
 			}
 		};
